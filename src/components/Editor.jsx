17a693e67b496139b8e5a8754a14a46947b4f7ad
Jhonathan Whales <jwhales.rosegrid@gmail.com>
2026-07-02 18:20:24 +0530

    feat: Add an editor column for personal details

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,6 +1,28 @@
+function EditorInput({name, id, labelText, inputType="text"}) {
+  return (
+      <div className="editor-input-wrapper">
+        <label htmlFor={id}>{labelText}</label>
+        <input className="editor-input" name={name} id={id} type={inputType} />
+      </div>
+  );
+}
+
+function EditorColumn() {
+  return (
+    <div className="editor-personal-details editor-column">
+      <p className="editor-column-header">Personal Details</p>
+      <EditorInput name="person_name" id="person-name" labelText="Full Name" />
+      <EditorInput name="person_phone" id="person-phone" labelText="Phone Number" />
+      <EditorInput name="person_mail" id="person-mail" labelText="Email" inputType="mail" />
+      <EditorInput name="person_location" id="person-location" labelText="Location" />
+    </div>
+  );
+}
+
 export default function Editor() {
   return (
     <section id="editor" className="editor-preview-column">
+      <EditorColumn />
     </section>
   );
 }
